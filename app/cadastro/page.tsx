4facab5887798f9f87a22d832f2afdeab0d38bdc
João Francisco <jhonjhon2sj@gmail.com>
2026-05-03 08:43:55 -0300

    Feat: Realizando a divisao do fomulario e da imagem

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -2,9 +2,25 @@ import "tailwindcss";
 
 export default function Cadastro() {
   return (
-    <main className="min-h-screen bg-[#f6f3e4] flex flex-col items-center justify-center p-8">
+      // Fundo bege claro da tela inteira
+      <div className="min-h-screen bg-[#f3f0e1] flex px-8 md:px-16 lg:px-24">
+        
+            //a div que vai organizar o lado esquerdo com formulario e o lado direito com as imagens
+          <div className="w-full flex flex-col md:flex-row justify-between items-end gap-8 pt-30">
+            
 
+              //O Formulário no lado esquerdo 
+            <div className="w-full md:w-[60%] xl:w-[35%] bg-[#171918] rounded-t-[40px] p-10 lg:p-24 shadow-2xl flex flex-col">
+                
 
-    </main>
+            </div>
+
+              //A imagem e o titulo no lado direito
+            <div className="hidden md:flex w-full md:w-[55%] flex-col items-center justify-center">
+
+            </div>
+
+          </div>
+      </div>
   );
 }
