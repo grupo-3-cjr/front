47c204ee3be4cc650c7170b79f2b5ee913a88ade
João Francisco <jhonjhon2sj@gmail.com>
2026-05-20 09:24:18 -0300

    feat: adicionando link na logo

--- components/feed/FeedNavbar.tsx
+++ components/feed/FeedNavbar.tsx
@@ -1,11 +1,14 @@
 import { User, LogOut } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function FeedNavbar() {
     return (
         <nav className="bg-black text-white flex items-center justify-between px-6 py-4">
-            <Image className="flex items-center" width={100} height={40} src="/STOCK.IO.svg" alt="Logo do site" />
-
+           {/*Link para voltar a página feed */}
+           <Link href="/feed">
+               <Image className="flex items-center" width={100} height={40} src="/STOCK.IO.svg" alt="Logo do site" />
+           </Link>
             <div className="flex gap-6 text-xl">
                 <span><User className="text-white" /></span>
                 <span><LogOut className="text-white" /></span>
