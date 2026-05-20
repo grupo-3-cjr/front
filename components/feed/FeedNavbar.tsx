2402fc17d8ec4de80c8fd1fab33646fdb2eece56
João Francisco <jhonjhon2sj@gmail.com>
2026-05-20 09:38:58 -0300

    feat: botoes login/cadastro

--- components/feed/FeedNavbar.tsx
+++ components/feed/FeedNavbar.tsx
@@ -2,16 +2,37 @@ import { User, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function FeedNavbar() {
+export default function FeedNavbar({ isLoggedIn = false }: { isLoggedIn?: boolean }) {
+
     return (
         <nav className="bg-black text-white flex items-center justify-between px-6 py-4">
            {/*Link para voltar a página feed */}
            <Link href="/feed">
                <Image className="flex items-center" width={100} height={40} src="/STOCK.IO.svg" alt="Logo do site" />
            </Link>
-            <div className="flex gap-6 text-xl">
-                <span><User className="text-white" /></span>
-                <span><LogOut className="text-white" /></span>
+           
+           <div className="flex items-center gap-6 font-semibold text-sm">
+
+                {/*Logado*/}
+                {isLoggedIn ? (               
+                    <>
+                        <span><User className="text-white" /></span>
+                         <span><LogOut className="text-white" /></span>
+                    </>
+                ) : (   //  Não logado
+                   <>
+                       <Link href="/login" className="transition-colors hover:text-purple-600">
+                           LOGIN
+                       </Link>
+                       <Link
+                           href="/cadastro"
+                           className="bg-purple-600 text-white px-5 py-2 rounded-full transition-colors hover:bg-white hover:text-purple-600"
+                       >
+                           CADASTRE-SE
+                       </Link>
+                   </>
+               )}
+
             </div>
         </nav>
     )
