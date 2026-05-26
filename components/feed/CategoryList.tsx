cc2cd94e99c528246a8ba43e327c489ea7a08c90
Leonardo Paiva da Costa Pereira <leonardo.pereira@SGB.LOCAL>
2026-05-26 14:49:09 -0300

    feat: integração das listas de categorias

--- components/feed/CategoryList.tsx
+++ components/feed/CategoryList.tsx
@@ -9,28 +9,40 @@ import {
   House,
 } from "lucide-react";
 
-const categories =[
-    { name: "Mercado", icon: ShoppingBasket },
-    { name: "Farmácia", icon: Pill},
-    { name: "Beleza", icon: Brush },
-    { name: "Moda", icon: Shirt },
-    { name: "Eletrônicos", icon: Laptop },
-    { name: "Jogos", icon: Gamepad2 },
-    { name: "Brinquedos", icon: ToyBrick},
-    { name: "Casa", icon: House }
-]
+type Category = {
+    id: number;
+    name: string;
+    parent_category_id: number | null;
+}
 
-export default function CategoryList() {
+type CategoryListProps = {
+    categories: Category[];
+}
+
+const iconMap: Record<string, any> = {
+  Mercado: ShoppingBasket,
+  Farmácia: Pill,
+  Beleza: Brush,
+  Moda: Shirt,
+  Eletrônicos: Laptop,
+  Jogos: Gamepad2,
+  Brinquedos: ToyBrick,
+  Casa: House,
+}
+
+export default function CategoryList({
+    categories,
+}: CategoryListProps) {
     return (
         <section className="mt-8 ml-16">
             <h2 className="text-3xl text-black font-bold mb-8">Categoria</h2>
 
             <div className="flex gap-15 overflow-x-auto pb-4">
                 {categories.map((category) => {
-                    const Icon = category.icon;
+                    const Icon = iconMap[category.name] || ShoppingBasket;
 
                     return (
-                        <button key={category.name} className="p-2 min-w-[115px] h-[115px] bg-white rounded-2xl flex flex-col items-center justify-center">
+                        <button key={category.id} className="p-2 min-w-[115px] h-[115px] bg-white rounded-2xl flex flex-col items-center justify-center">
 
                             <Icon size={30} className="text-[#6C3BFF]" />
                             <span className="text-black text-sm font-medium">
