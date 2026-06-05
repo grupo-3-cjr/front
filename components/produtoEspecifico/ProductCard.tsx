965cf1c53834278abc11b4fd48032a7ab0cb5c17
João Francisco <jhonjhon2sj@gmail.com>
2026-06-05 12:33:48 -0300

    feat: integrando a parte de produtos da mesma loja ao back

--- components/produtoEspecifico/ProductCard.tsx
+++ components/produtoEspecifico/ProductCard.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
+
 type ProductCardProps = {
+    id: number;
     name: string;
     image: string;
+    description?: string; 
+    price?: string;   
     storeLogo?: string;
 }
 
-export default function ProductCard({name, image, storeLogo}: ProductCardProps) {
+export default function ProductCard({id, name, image, description, price, storeLogo}: ProductCardProps) {
+    console.log(`Card do ${name} recebeu o preço:`, price);
+
     return(
+        <Link href={`/produto/${id}`} className="block group">   {/* redireciona */}
+
         <article className="min-w-[228px] h-[280px] bg-white rounded-[28px] relative overflow-hidden flex items-center justify-center flex-col">
             {storeLogo && (
                 <img
@@ -21,8 +30,20 @@ export default function ProductCard({name, image, storeLogo}: ProductCardProps) 
                 className="w-[150px] h-[170px] pt-8 object-contain -translate-y-12 z-5"
             />
 
-            <span className="font-semibold text-black text-lg -translate-y-8">{name}</span>
+         <div className="flex flex-col items-center justify-center w-full px-4 text-center -translate-y-2">
+                <span className="font-semibold text-black text-lg line-clamp-1" title={name}>
+                    {name}
+                </span>
+                
+                {/* Renderiza o preço se ele existir */}
+                {price && (
+                    <span className="font-bold text-[#6A38F3] text-base mt-1">
+                        {price}
+                    </span>
+                )}
+            </div>
             
         </article>
+        </Link>
     );
 }
