f4eb8fca5b44a71d8bfd8d2ecc4b86d8c94c9e88
João Francisco <jhonjhon2sj@gmail.com>
2026-06-18 20:05:04 -0300

    fix: produto no tamanho adequado

--- components/usuario/ProductCard.tsx
+++ components/usuario/ProductCard.tsx
@@ -7,6 +7,7 @@ type ProductCardProps = {
     price: string;
     image_url?: string;
     storeLogo?: string;
+    available: boolean;
 }
 
 export default function ProductCard({
@@ -16,35 +17,35 @@ export default function ProductCard({
     price,
     image_url,
     storeLogo,
+    available,
 }: ProductCardProps) {
     return(
         <Link href={`/produto/${id}`}>
 
-            <article className="relative bg-white min-w-[228px] h-[280px] rounded-[28px] overflow-hidden flex items-center justify-center flex-col">
-
-                {/* {storeLogo && (
-                    <img
-                        src={storeLogo}
-                        alt="Logo da loja"
-                        className="absolute top-2 right-4 w-14 h-14 rounded-full object-cover z-10"
-                    />
-                )} */}
-
+            <article className="relative bg-white w-[228px] h-[300px] rounded-[28px] overflow-hidden flex items-center justify-center flex-col">
                 <img
-                    src={image_url || "/brownie.jpg"}
-                    alt={name}
-                    className="w-[150px] h-[170px] pt-8 object-contain -translate-y-12 z-5"
-                />
-                <img
-                    src={storeLogo || "/brownie.jpg"}
+                    src={storeLogo || "/globe.svg"}
                     alt="Logo da loja"
                     className="absolute top-2 right-4 w-14 h-14 rounded-full object-cover z-10"
                 />
-                <span className="font-semibold text-black text-lg -translate-y-8">{name}</span>
+                
+                <div className="h-[155px] flex items-center justify-center pt-2">
+                    <img
+                        src={image_url || "/semImagemProduto.jpg"}
+                        alt={name}
+                        className="w-[150px] h-[170px] object-contain z-5"
+                    />
+                </div>
+                
+                <div className="mt-2 flex flex-col items-start p-3">
+                    <span className="font-semibold text-black text-lg">{name}</span>
 
-                <span className="font-semibold text-black text-lg -translate-y-7 truncate w-full text-center px-4">{description}</span>
+                    <span className="font-semibold text-black text-xl">R$ {price}</span>
 
-                <span className="font-bold text-[#6A38F3] text-xl -translate-y-6">R$ {price}</span>
+                    <p className={available ? "text-[#AACC00] text-sm font-semibold pb-2" : "text-red-500 text-sm font-semibold pb-2"}>
+                        {available ? "DISPONÍVEL" : "INDISPONÍVEL"}
+                    </p>
+                </div>
             </article>
 
         </Link>
